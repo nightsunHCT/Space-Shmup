6d76d32c2e4938bab9738951ab4d711b80809222
Helena <nightsunHCT@users.noreply.github.com>
2020-05-07 18:15:12 -0400

    Hero Script

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+    // this fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
     {
-        
+        if (S == null)
+        {
+            S = this; // set the singleton
+        } else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // pull in infomation from the INput class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        // Chnage transform.position based on axis
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
     }
 }
