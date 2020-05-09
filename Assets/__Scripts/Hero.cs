e2085763bb7529ad3663868089e312f0b1b3803d
Helena <nightsunHCT@users.noreply.github.com>
2020-05-09 14:15:27 -0400

    Weapon Firing

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -31,7 +31,7 @@ public class Hero : MonoBehaviour
         {
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
-        fireDelegate += TempFire;
+        // fireDelegate += TempFire;
     }
 
     void Update()
